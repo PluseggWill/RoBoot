7f69d4299323da2126368abae3bf62e2d1a65127
PluseggWill <54594803+PluseggWill@users.noreply.github.com>
2020-02-02 12:30:54 -0500

    SE added

---
+++ Assets/Scripts/SoundEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect : MonoBehaviour
+{
+    public AudioSource[] m_AudioSources;
+    private RoBoot m_RoBoot;
+
+    public void PlayAudio(int num)
+    {
+        m_AudioSources[num].Play();
+    }
+
+    public void StopAudio(int num)
+    {
+        m_AudioSources[num].Stop();
+    }
+}
